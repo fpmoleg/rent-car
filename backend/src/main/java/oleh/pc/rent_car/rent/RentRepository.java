0b1db6863d037bbf36f3125dbc2f481e1dba06ee
Oleh <fpmoleg@gmail.com>
2024-11-11 19:14:16 +0100

    Clear all.

--- backend/src/main/java/oleh/pc/rent_car/rent/RentRepository.java
+++
@@ -1,6 +0,0 @@
-package oleh.pc.rent_car.rent;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RentRepository extends JpaRepository<Rent, Long> {
-}
--- backend/src/main/resources/db/migration/structure/V2__create_user.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE IF NOT EXISTS users (
-    id             int auto_increment primary key,
-    full_name      varchar(50) not null,
-    driver_licence varchar(15) not null,
-    constraint users_driver_licence_unq unique (driver_licence)
-)
